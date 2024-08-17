830853d6208c5b8babe2ca11673c6d0081d7d163
marcus-gerrard <mgerrard@ualberta.ca>
2024-08-17 15:58:16 -0600

    finished store

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+      totalCost += parseInt(item.cost.substring(1)) * item.quantity;
+    });
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    let newTotal = item.quantity - 1
+    if (newTotal > 0) {
+      dispatch(updateQuantity({name: item.name, quantity: newTotal}));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    cart.forEach((plant) => {
+      if (plant.name === item.name) {
+        totalCost += parseInt(plant.cost.substring(1)) * plant.quantity;
+      }
+    });
+    return totalCost;
   };
 
   return (
